b0e7105f980ea54e9e08d118df526bf380408e11
i-love-linux <1137247975@qq.com>
2020-06-19 16:31:24 +0800

    系列1 全部完成

--- JVM系列之1：内存与垃圾回收篇/JVMDemo/chapter13/src/com/atguigu/java2/StringExer2.java
+++ JVM系列之1：内存与垃圾回收篇/JVMDemo/chapter13/src/com/atguigu/java2/StringExer2.java
@@ -7,10 +7,14 @@ package com.atguigu.java2;
  */
 public class StringExer2 {
     public static void main(String[] args) {
-        String s1 = new String("ab");//执行完以后，会在字符串常量池中会生成"ab"
-//        String s1 = new String("a") + new String("b");////执行完以后，不会在字符串常量池中会生成"ab"
+        String s1 = new String("ab");  // 执行完以后，会在字符串常量池中会生成"ab"
         s1.intern();
         String s2 = "ab";
-        System.out.println(s1 == s2);
+        System.out.println(s1 == s2);  // jdk6/7/8：false
+		
+//        String s1 = new String("a") + new String("b");  // 执行完以后，不会在字符串常量池中会生成"ab"
+//        s1.intern();
+//        String s2 = "ab";
+//        System.out.println(s1 == s2);  // jdk6：false		jdk7/8：true
     }
 }

66fc5b3a7e1b3dc3d2f361f5d984feba08d2804f
wxx <1137247975@qq.com>
2020-10-12 19:59:53 +0800

    JVM 2 类型转换指令

--- JVM系列之2：字节码与类的加载篇/代码/JVMDemo1/chapter02/src/com/atguigu/java/ClassCastTest.java
+++ JVM系列之2：字节码与类的加载篇/代码/JVMDemo1/chapter02/src/com/atguigu/java/ClassCastTest.java
@@ -6,107 +6,107 @@ import org.junit.Test;
 /**
  * @author shkstart
  * @create 2020-09-08 0:11
- *
+ * -XX:+TraceClassLoading
+ * <p>
  * 指令3：类型转换指令
  */
 public class ClassCastTest {
-    //宽化类型转换
+    // 宽化类型转换
 
-    //针对于宽化类型转换的基本测试
-    public void upCast1(){
-        int i  = 10;
-        long l = i;
-        float f = i;
-        double d = i;
+    // 针对于宽化类型转换的基本测试
+    public void upCast1() {
+        int i = 10;
+        long l = i;  // i2l
+        float f = i;  // i2f
+        double d = i;  // i2d
 
-        float f1 = l;
-        double d1 = l;
+        float f1 = l;  // l2f
+        double d1 = l;  // l2d
 
-        double d2 = f1;
+        double d2 = f1;  // f2d
     }
-    //举例：精度损失的问题
+
+    // 举例：精度损失的问题
     @Test
-    public void upCast2(){
+    public void upCast2() {
         int i = 123123123;
         float f = i;
-        System.out.println(f);//123123120
+        System.out.println(f);  // 123123120
 
         long l = 123123123123L;
         l = 123123123123123123L;
         double d = l;
-        System.out.println(d);//123123123123123120
-
+        System.out.println(d);  // 123123123123123120
     }
-    //针对于byte、short等转换为容量大的类型时，将此类型看做int类型处理。
-    public void upCast3(byte b){
+
+    // 针对于byte、short等转换为容量大的类型时，将此类型看做int类型处理。
+    public void upCast3(byte b) {
         int i = b;
-        long l = b;
-        double d = b;
+        long l = b;  // i2l
+        double d = b;  // i2d
     }
 
-    public void upCast4(short s){
+    public void upCast4(short s) {
         int i = s;
-        long l = s;
-        float f = s;
+        long l = s;  // i2l
+        float f = s;  // i2f
     }
 
-    //窄化类型转换
-    //基本的使用
-    public void downCast1(){
+    // 窄化类型转换
+    // 基本的使用
+    public void downCast1() {
         int i = 10;
-        byte b = (byte)i;
-        short s = (short)i;
-        char c = (char)i;
+        byte b = (byte) i;  // i2b
+        short s = (short) i;  // i2s
+        char c = (char) i;  // i2c
 
         long l = 10L;
-        int i1 = (int)l;
-        byte b1 = (byte) l;
-
-
+        int i1 = (int) l;  // l2i
+        byte b1 = (byte) l;  // l2i --> i2b
     }
-    public void downCast2(){
+
+    public void downCast2() {
         float f = 10;
-        long l = (long)f;
-        int i = (int)f;
-        byte b = (byte)f;
+        long l = (long) f;  // f2l
+        int i = (int) f;  // f2i
+        byte b = (byte) f;  // f2i --> i2b
 
         double d = 10;
-        byte b1 = (byte)d;
+        byte b1 = (byte) d;  // d2i --> i2b
 
     }
 
-    public void downCast3(){
+    public void downCast3() {
         short s = 10;
-        byte b = (byte)s;
+        byte b = (byte) s;  // i2b
     }
 
     //窄化类型转换的精度损失
     @Test
-    public void downCast4(){
+    public void downCast4() {
         int i = 128;
-        byte b = (byte)i;
-        System.out.println(b);
+        byte b = (byte) i;
+        System.out.println(b);  // -128
     }
-    //测试NaN,无穷大的情况
+
+    // 测试NaN,无穷大的情况
     @Test
-    public void downCast5(){
-        double d1 = Double.NaN; //0.0 / 0.0
-        int i = (int)d1;
-        System.out.println(d1);
-        System.out.println(i);
+    public void downCast5() {
+        double d1 = Double.NaN;  // 0.0 / 0.0
+        int i = (int) d1;
+        System.out.println(d1);  // NaN
+        System.out.println(i);  // 0
 
         double d2 = Double.POSITIVE_INFINITY;
-        long l = (long)d2;
-        int j = (int)d2;
-        System.out.println(l);
-        System.out.println(Long.MAX_VALUE);
-        System.out.println(j);
-        System.out.println(Integer.MAX_VALUE);
+        long l = (long) d2;
+        int j = (int) d2;
+        System.out.println(l);  // Long.MAX_VALUE
+        System.out.println(j);  // Integer.MAX_VALUE
 
-        float f = (float)d2;
-        System.out.println(f);
+        float f = (float) d2;
+        System.out.println(f);  // Infinity
 
-        float f1 = (float)d1;
-        System.out.println(f1);
+        float f1 = (float) d1;
+        System.out.println(f1);  // NaN
     }
 }
